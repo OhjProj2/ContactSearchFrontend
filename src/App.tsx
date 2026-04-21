a708ae5641f80d9b3c41bf5cec603228316f0bac
Prshkv <niko.prshkv@gmail.com>
2026-04-21 15:33:07 +0300

    small fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import {Routes, Route} from 'react-router-dom';
 
 import Home from './pages/home';
-import Databases from './pages/coming_soon';
 import { MainLayout } from './layouts/MainLayout';
+import ComingSoon from './pages/coming_soon';
 
 function App() {
   
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/coming_soon" element={<Databases />} />
+          <Route path="/coming_soon" element={<ComingSoon />} />
         </Route>
       </Routes>
     </>

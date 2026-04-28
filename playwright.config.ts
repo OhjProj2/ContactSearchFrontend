a1499b735c2df75ea66d06bcfacc2a214b95e11b
veera.elovaara@myy.haaga-helia.fi <veera.elovaara@myy.haaga-helia.fi>
2026-04-28 20:30:26 +0300

    Fix CI Playwright server timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,8 +72,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run build && npm run preview',
+    command: 'npm run preview -- --port 5173 --strictPort',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
-  },
+    timeout: 120000,
+  }
 });
